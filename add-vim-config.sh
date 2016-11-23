0eac96cbe14bbb9f330aec893f56d68f8a7f40ca
Daniel Mota <daniel.mota@wavecode.com>
2016-11-23 14:35:41 -0500

    Added automatic vim config adding script

---
+++ add-vim-config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export VIMDIR=~/.vim
+export VIMFILE=~/.vimrc
+if [ -f $VIMFILE ]; then
+	echo "You already have a $VIMFILE! Cancelling!"
+	exit 1
+fi
+if [ -d $VIMDIR ]; then 
+	echo "You already have a $VIMDIR directory! Cancelling!"
+	exit 1
+fi
+git clone https://github.com/d-mota/vim.git $VIMDIR
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s $VIMDIR/init.vim $VIMFILE
+
